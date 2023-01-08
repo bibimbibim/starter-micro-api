c6b507206a900d1903682e5f077ca02106489303
비빔 <79782169+bibimbibim@users.noreply.github.com>
2023-01-08 18:34:30 +0000

    change

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const { WeverseClient } = require("weverse");
+
+myToken = process.env.myToken
+const myClient = new WeverseClient({ token: myToken })
+myClient.init({ allPosts: true, allNotifications: false })
+
+console.log("실행!")
+
+myClient.on('init', async (ready) => {
+    if (ready) {
+        myClient.listen({ listen: true, interval: 5000 })
+    }
+})
+
+myClient.on('comment', (comment, post) => {
+    // all objects are typed
+    console.log("comment: ", comment)
+    const commenter = myClient.artistById(comment.artist.id)
+    console.log("commenter: ", commenter)
+    const postAuthor = myClient.artistById(post.artist.id)
+    console.log(`${commenter.name} commented on ${postAuthor.name}'s post!`)
+})
+
+myClient.on('post', (post) => {
+    if (post.photos.length) {
+        post.photos.forEach(photo => {
+            downloadImage(photo.orgImgUrl)
+        })
+    }
+})
+
+myClient.on('notification', (notification) => {
+    console.log(notification)
+})
